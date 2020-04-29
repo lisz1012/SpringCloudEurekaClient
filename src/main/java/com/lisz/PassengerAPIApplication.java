d455aac43916199d3acbbdbbe2e9cb4b94515726
lisz1012 <lisz1012@163.com>
2020-04-28 22:17:18 -0700

    Block the process of the PassengerAPIApplication to see it in registration center

--- src/main/java/com/lisz/PassengerAPIApplication.java
+++ src/main/java/com/lisz/PassengerAPIApplication.java
@@ -4,10 +4,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+import java.io.IOException;
+
 @EnableEurekaClient
 @SpringBootApplication
 public class PassengerAPIApplication {
     public static void main( String[] args ) {
         SpringApplication.run(PassengerAPIApplication.class, args);
+        try {
+            System.in.read();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
