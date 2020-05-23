48a69a77f99b93716c8fdb4cec66b68e0cd6455f
lisz1012 <lisz1012@163.com>
2020-05-23 01:15:16 -0700

    Add notes

--- src/main/java/com/lisz/service/remote/IndexService.java
+++ src/main/java/com/lisz/service/remote/IndexService.java
@@ -4,7 +4,12 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-// Service接口，代圣贤（服务提供方的Controller）立言！指定了url就会访问该url地址，否则会把name参数当作服务名到注册中心中查询该名字服务，此时指定了url后可以随意命名
+/*
+Service接口，代圣贤（服务提供方的Controller）立言！指定了url就会访问该url地址，否则会把name参数当作服务名到注册中心中查询该名字服务，此时指定了url后可以随意命名
+Feign原理：我们首先会添加@EnableFeignClients注解开启对 FeignClient扫描加载处理，扫描后会注入到SpringIOC容器，当定义的feign接口方法被调用时，通过JDK代理的方式，
+生成具体的RequestTemplate，RequestTemplate生成Request，交给URLConnection处理，并结合LoadBalanceClient与Ribbon，以负载均衡的方式发起服务间的调用
+原文链接：https://blog.csdn.net/zhuyu19911016520/java/article/details/84933268
+ */
 @FeignClient(name = "search-github", url="https://api.github.com")
 public interface IndexService {
     @GetMapping("/search/repositories")
--- src/main/java/com/lisz/service/remote/SMSService.java
+++ src/main/java/com/lisz/service/remote/SMSService.java
@@ -6,7 +6,12 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-// Service接口，代圣贤（服务提供方的Controller）立言！这里没有指定url，会访问Eureka上name是"sms-service"的服务，该服务必须已注册
+/*
+ Service接口，代圣贤（服务提供方的Controller）立言！这里没有指定url，会访问Eureka上name是"sms-service"的服务，该服务必须已注册
+ Feign原理：我们首先会添加@EnableFeignClients注解开启对 FeignClient扫描加载处理，扫描后会注入到SpringIOC容器，当定义的feign接口方法被调用时，通过JDK代理的方式，
+ 生成具体的RequestTemplate，RequestTemplate生成Request，交给URLConnection处理，并结合LoadBalanceClient与Ribbon，以负载均衡的方式发起服务间的调用
+ 原文链接：https://blog.csdn.net/zhuyu19911016520/java/article/details/84933268
+ */
 @FeignClient(name = "sms-service")
 public interface SMSService {
     @PostMapping("/sms/metadata") // 跟Controller中的restTemplate.postForEntity调用的是同一个SMSService的Rest API，这里用了Feign，更加OOD
