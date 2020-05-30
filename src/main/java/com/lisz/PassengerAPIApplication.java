b136399e7dc636095e9a7754a66540c4070815a8
lisz1012 <lisz1012@163.com>
2020-05-29 21:40:35 -0700

    Use ribbon without eureka

--- src/main/java/com/lisz/PassengerAPIApplication.java
+++ src/main/java/com/lisz/PassengerAPIApplication.java
@@ -3,14 +3,14 @@ package com.lisz;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 import java.io.IOException;
 
-@EnableEurekaClient
+//@EnableEurekaClient
 @SpringBootApplication
 @EnableFeignClients
 public class PassengerAPIApplication {
